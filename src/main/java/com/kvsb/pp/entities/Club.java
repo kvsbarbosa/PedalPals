2f3c88d835dc7b4da5edb68d38f799fbddc10c63
Kaio Vinícius <kaiovsbarbosa@gmail.com>
2024-03-21 00:34:46 -0300

    add entity Club

---
+++ src/main/java/com/kvsb/pp/entities/Club.java
@@ -0,0 +1,29 @@
+package com.kvsb.pp.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "clubs")
+public class Club {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String photoUrl;
+    private String content;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @CreationTimestamp
+    private LocalDateTime updateOn;
+
+}
