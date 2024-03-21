8cb34c448bd2a50f78e9abb35acc47602feb92c1
Kaio Vinícius <kaiovsbarbosa@gmail.com>
2024-03-21 00:12:19 -0300

    Initial commit

---
+++ src/main/java/com/kvsb/csmvc/CsmvcApplication.java
@@ -0,0 +1,13 @@
+package com.kvsb.csmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kvsb/csmvc/CsmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kvsb.csmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
